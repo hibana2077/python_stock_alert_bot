7b7c3dd46240778f7bf9f7cfa0ab90f0951c3dc5
hibana2077 <hibana2077@gmail.com>
2023-07-12 20:57:25 +0800

    add some cool func

---
+++ src/lab.py
@@ -0,0 +1,5 @@
+# import pandas as pd
+
+# df = pd.read_csv("https://datahub.io/core/nasdaq-listings/r/1.csv")
+# symbols = df["Symbol"].tolist()
+# print(f"symbols type: {type(symbols)} , symbols length: {len(symbols)}")
